463dfab93d958bda46001e3520675f0c7fa639a3
Mehran <i@mehrankhan.com>
2016-09-02 11:14:33 -0400

    second, different attempt to automate starting ME/osaf

--- ME_osaf_starter.sh
+++ ME_osaf_starter.sh
@@ -2,3 +2,4 @@
 cd /home/node1/Downloads/lttngAnalysesForOpenSAF/
 echo magic123 | sudo -S python3 -m lttngAnalyses.destroy_and_clean
 echo magic123 | sudo -S python3 -m lttngAnalyses.daemon_controller_client 192.168.10.7 6666 &
+echo magic123 | sudo -S service opensafd start
