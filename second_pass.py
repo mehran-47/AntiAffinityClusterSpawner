82cd5e1d014cec1e1d41f5daf56fce55918359a1
Mehran <i@mehrankhan.com>
2016-09-14 12:29:48 -0400

    finally ME works at boot time, second pass is redundant

--- second_pass.py
+++ second_pass.py
@@ -1,14 +1,23 @@
 from vm_spawner import VMSpawner
+from threading import Thread
 import glob, json, os
 
 def second_pass(vms):
 	for configPath in sorted(glob.glob('./vm_config_*')):
 		with open(configPath, 'r') as f: aVMConfig = json.loads(f.read())
+		Thread(target= vms.execute_commands_at, args=( \
+			aVMConfig['username'], \
+			vms.get_floating_ip_of(aVMConfig['openStackVMname']), \
+			aVMConfig['password'], \
+			['echo ' + aVMConfig['password'] + ' | sudo -S service default_startups start'] )
+		).start()
+		'''
 		vms.execute_commands_at( \
 			aVMConfig['username'], \
 			vms.get_floating_ip_of(aVMConfig['openStackVMname']), \
 			aVMConfig['password'], \
-			['echo ' + aVMConfig['password'] + ' | sudo -S service default_startups start', 'exit'] )
+			['echo ' + aVMConfig['password'] + ' | sudo -S service default_startups start'] )
+		'''
 
 if __name__=='__main__':
 	if os.path.exists(os.getcwd()+'/config.json'):
