6431f39fefccf3056a071d8ee63cf78a84eeec39
Mehran <i@mehrankhan.com>
2016-09-20 16:02:50 +0000

    updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,11 @@ rm vm_config_*
 nova delete AF_VM_1
 nova delete AF_VM_2
 nova delete AF_VM_3
+nova delete AF_VM_5
+nova delete AF_VM_6
+nova delete AF_VM_7
 nova delete AF_VM_4
+nova delete AF_VM_8
 nova server-group-list
 echo "nova server-group-delete"
 
