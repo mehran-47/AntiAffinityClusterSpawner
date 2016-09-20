3c73a6a03af5927b7e8b766e1e2d9ae16498c00d
Mehran <i@mehrankhan.com>
2016-09-20 17:30:55 -0400

    reversed VM cleanup order (payloads first, controllers next), changed preferred number of assignments for each SI to 4

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 rm vm_config_* 
-nova delete AF_VM_1
-nova delete AF_VM_2
-nova delete AF_VM_3
-nova delete AF_VM_5
-nova delete AF_VM_6
-nova delete AF_VM_7
-nova delete AF_VM_4
 nova delete AF_VM_8
+nova delete AF_VM_7
+nova delete AF_VM_6
+nova delete AF_VM_5
+nova delete AF_VM_4
+nova delete AF_VM_3
+nova delete AF_VM_2
+nova delete AF_VM_1
 nova server-group-list
 echo "nova server-group-delete"
 
